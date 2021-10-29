105ae0799a5db2018fc35221898c10a72af22576
RamejaAyush <Ayushrwork@gmail.com>
2021-10-29 12:46:39 +0530

    changes in dashboard and services

---
+++ src/app/services/items.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { ITEM } from './ITEMS';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ItemsService {
+  localItem: string;
+  Items: ITEM[];
+
+  constructor(private router: Router) {
+    this.localItem = localStorage.getItem('items');
+    if (this.localItem == null) {
+      this.Items = [];
+    } else {
+      this.Items = JSON.parse(this.localItem);
+    }
+  }
+
+  addItem(enteredName, enteredDesc, enteredPrice) {
+    if (
+      enteredName.value == '' ||
+      enteredDesc.value == '' ||
+      enteredPrice.value == 0
+    ) {
+      alert('Enter Valid Values');
+    } else {
+      let item: ITEM = {
+        id: this.Items.length + 1,
+        name: enteredName.value,
+        desc: enteredDesc.value,
+        price: enteredPrice.value,
+      };
+      this.Items.push(item);
+      localStorage.setItem('items', JSON.stringify(this.Items));
+      alert('changes are saved in localstorage');
+      enteredName.value = '';
+      enteredDesc.value = '';
+      enteredPrice.value = 0;
+      this.router.navigate(['dashboard']);
+      alert('task is created');
+    }
+  }
+
+  deleteItem(item: ITEM) {
+    const index = this.Items.indexOf(item);
+    this.Items.splice(index, 1);
+    localStorage.setItem('items', JSON.stringify(this.Items));
+    alert('Item removed');
+  }
+}
